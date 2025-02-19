ad1fc8869a4c92380e9d2156a35101df749cbe2a
evaaabruh <evamariucaradulescu1@gmail.com>
2025-02-19 23:19:52 +0200

    animations , attacking enemy, taking damage from enemy
    (hit animation is looped for some reason and it shouldnt be)

---
+++ Assets/Scripts/PlayerMeleeCombat.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Transform attackPos;
+    public float attackRange = 1f;
+    public LayerMask enemyLayer;
+    private float Cooldown = 2f;
+    [SerializeField] private float _startCooldown;
+    public int damage = 1;
+    private int _lives = 3;
+    private Animator _animator;
+    [SerializeField] private Animator Player;
+
+    private static readonly int Attack = Animator.StringToHash("Attack");
+            private static readonly int isHit = Animator.StringToHash("isHit");
+
+
+
+
+    void Start()
+    {
+        damage = 1;
+        
+        _animator = GetComponent<Animator>(); 
+        _animator.SetBool(Attack, false);
+                _animator.SetBool(isHit, false);
+
+    }
+    void Update()
+    {
+        if (Cooldown <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                {
+                    _animator.SetTrigger("Attack"); 
+                                        
+
+                    
+                }
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemyLayer);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    //Debug.Log("damage"+damage);
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                    enemiesToDamage[i].GetComponent<Box>().BoxTakeDamage(damage);
+                }
+
+            }
+            _animator.SetBool(Attack, false);
+
+
+            Cooldown = _startCooldown;
+        }
+        else
+        {
+            Cooldown -= Time.deltaTime;
+        }
+    }
+
+    public void PlayerTakeDamage(int damageToPlayer)
+    {
+        _lives -= 1;
+        _animator.SetTrigger("isHit"); 
+        if (_lives <= 0)
+        {
+            Debug.Log("Player Died :C"); //aici se schimba scena in aia cu Game Over 
+        }
+
+    }
+
+}
+
+
+
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,65 +2,81 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    public float Test = 1;
-    public float Speed = 2;
-    public float jumpForce = 5;
+    public float Speed = 5f;
+    public float jumpForce = 8f;
     private Rigidbody2D _rigidbody2D;
-
     private float _horizontal;
-    private Collider2D _collision;
-    
-    private bool _isGrounded=true;
-    
+    private SpriteRenderer spriteRenderer;
+    private Animator _animator;
+    private bool _isGrounded = true;
+    private static readonly int isMoving = Animator.StringToHash("isMoving");
+    private static readonly int isJumping = Animator.StringToHash("isJumping");
+
 
 
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _rigidbody2D.freezeRotation = true;
+        _animator = GetComponent<Animator>();
+        _animator.SetBool(isJumping, false);
+    }
+
+    void Update()
+    {
+        // Handle movement input
+        _horizontal = Input.GetAxisRaw("Horizontal");
+
+        // Jump input handled here to prevent missed inputs
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            Jump();
+            _animator.SetBool(isJumping, true);
+        } 
+         _animator.SetBool(isJumping, !_isGrounded);
     }
 
     void FixedUpdate()
     {
-        _horizontal = Input.GetAxisRaw("Horizontal");
-
-        
+        // Apply movement
+        _rigidbody2D.linearVelocity = new Vector2(_horizontal * Speed, _rigidbody2D.linearVelocity.y);
         if (_horizontal != 0)
         {
-            _rigidbody2D.linearVelocity = new Vector2(_horizontal * Speed, _rigidbody2D.linearVelocity.y);
-
+            _animator.SetBool(isMoving, true);
         }
-        
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded == true)
+        else
         {
-            Jump();
+            _animator.SetBool(isMoving, false);
 
         }
+        if (_horizontal < 0)
+        {
+            Vector3 playerScale = transform.localScale;
+            playerScale.x = playerScale.x*1;
+        }
 
-       
     }
-     void Jump()
-        {
 
-            _rigidbody2D.linearVelocity = new Vector2(_rigidbody2D.linearVelocity.x, jumpForce);
-            _isGrounded=false;
-        }
-    private void OnTriggerEnter2D(Collider2D _collision)
+    void Jump()
     {
-       
-    if (_collision.gameObject.CompareTag("Ground"))
+        _rigidbody2D.linearVelocity = new Vector2(_rigidbody2D.linearVelocity.x, jumpForce);
+        _isGrounded = false; 
+    
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
         {
             _isGrounded = true;
         }
     }
-    private void OnTriggerExit2D(Collider2D _collision)
+
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        
-    if (_collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
             _isGrounded = false;
         }
-        
     }
 }
